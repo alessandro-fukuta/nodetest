d3dc3686c53243dbdcf183c11533694df8decf63
Ubuntu <fukuta@server.o5wy5riahw2etjnr2cbdgpauib.cx.internal.cloudapp.net>
2025-04-03 14:20:51 -0300

    fatec franca dsm

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set('view engine', 'ejs');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+
+app.get("/", (req,res) => { 
+
+	res.render("index");
+
+});
+
+
+app.get("/clientes", (req,res) => {
+    res.render("clientes");
+});
+
+
+app.listen(3000, ()=> {
+    console.log("conectado na porta 3000");
+})
