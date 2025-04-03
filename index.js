415f086880fd5a23cca9235d76160986790bc98d
Your Name <fukuta2003@hotmail.com>
2025-04-03 15:43:35 -0300

    atualizaçoes

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.get("/clientes", (req,res) => {
 });
 
 
-app.listen(3000, ()=> {
-    console.log("conectado na porta 3000");
+app.listen(4000, ()=> {
+    console.log("conectado na porta 4000");
 })
