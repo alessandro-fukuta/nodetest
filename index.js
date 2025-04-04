3c86055c489c73bb59b372ab35dc863e90caae06
Your Name <fukuta2003@hotmail.com>
2025-04-04 15:31:27 -0300

    04/04/2025

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-//const database = require("./database/database");
-const connection = require("./database/connection");
-const porta = 3000;
+const express = require("express"); // importando o express
+const app = express(); // definindo o objeto app como express
+const bodyParser = require("body-parser"); // importando o body-parser
+const connection = require("./database/connection"); // conexao com banco de dados
+const porta = 4000; // porta que vai rodar a aplicação
 
+// definindo a view principal do express com ejs
 app.set('view engine', 'ejs');
+
+// definindo a pasta publica de arquivos estáticos
+app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(express.static('public'));
-
 // parse application/json
 app.use(bodyParser.json())
 
-
+// definindo a rota principal atraves de um controller
 const homeController = require("./home/homeController");
 app.use("/",homeController);
 
+// escutando a porta tornando a aplicação acessível
 app.listen(porta, ()=> {
     console.log("conectado na porta: " + porta);
 })
