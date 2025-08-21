30abf1c4d64ba8493f2ddf101840e66690c2353d
fukuta2003@hotmail.com <fukuta2003@hotmail.com>
2025-08-21 16:29:55 -0300

    second commit

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql2");
 var connection = mysql.createConnection({
     host:"localhost",
     user:"fukuta",
-    password:"fpc250272#",
+    password:"FatecFranca123#",
     database: "MINHA_API"
 });
 
